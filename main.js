ec411ca367d7c3e8dbf5a8b59d8f93b3d08fe947
daiyy <behoya11@gmail.com>
2023-07-14 20:20:28 +0800

    modify mobile button effect

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-document
-  .querySelectorAll(".jumping")
-  .forEach(
-    (button) =>
-      (button.innerHTML =
-        "<div><span>" +
-        button.textContent.trim().split("").join("</span><span>") +
-        "</span></div>")
-  );
+document.querySelectorAll(".jumping").forEach((button) =>
+  // 將 button 字母分開，並使用 span 包起來，最外圍用一個 div 包起來。
+  {
+    button.innerHTML =
+      "<div><span>" +
+      button.textContent.trim().split("").join("</span><span>") +
+      "</span></div>";
+  }
+);
